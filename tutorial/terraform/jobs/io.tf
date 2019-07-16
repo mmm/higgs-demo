b74881a39d1cd3238a0618fc32eb36f7e82d7da6
Mark Mims <mark.mims@gmail.com>
2019-07-15 18:08:01 -0600

    adding job template

--- tutorial/terraform/jobs/io.tf
+++ tutorial/terraform/jobs/io.tf
@@ -1,4 +1,30 @@
 
+variable "sm11_job_config" {
+  type = "map"
+  default = {
+    CMS_LUMINOSITY_DATA = "null"
+    CMS_JSON = "null"
+    CMS_INPUT_FILES = "null"
+    CMS_OUTPUT_FILE = "/tmp/output.root"
+    CMS_OUTPUT_JSON_FILE = "/tmp/output.json"
+    CMS_S3_BASEDIR = "gs/higgs-tutorial/testoutputs/higgs4lbucket/run6/eventselection"
+    CMS_OUTPUT_S3PATH = ""
+    CMS_CONFIG = "/configs/demoanalyzer_cfg_level4MC.py"
+    CMS_DATASET_NAME = ""
+    MC_MULTIPART_THREADS = "10"
+    S3_ACCESS = ""
+    S3_SECRET = ""
+    S3_HOST = ""
+    GCS_ACCESS = ""
+    GCS_SECRET = ""
+    GCS_HOST = "https://storage.googleapis.com"
+    GCS_PROJECT_ID = ""
+    DOWNLOAD_MAX_KB = "50000"
+    UPLOAD_MAX_KB = "10000"
+    REDIS_HOST = "higgs-redis-svc.default.svc.cluster.local"
+    DPATH = "/dev/shm"
+  }
+}
 #variable "kubeconfig" {
   #default = ""
 #}
@@ -40,3 +66,5 @@
 #output "lb_ip" {
   #value = "${kubernetes_service.heater.load_balancer_ingress.0.ip}"
 #}
+
+
